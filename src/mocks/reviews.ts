3caa50c20ddc426b30f619f9c9a8e999086f06d3
alyona.belova <magic.near@yandex.ru>
2025-11-30 18:11:40 +0300

    feat: add reviews components, integrate map on offer page, reuse offer list for nearby places

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,24 @@
+export const reviews = [
+  {
+    id: 1,
+    user: {
+      name: 'Max',
+      avatarUrl: 'img/avatar-max.jpg',
+    },
+    rating: 4,
+    comment:
+      'A quiet cozy and picturesque place that hides behind a river by the unique lightness of Amsterdam.',
+    date: '2024-05-08',
+  },
+  {
+    id: 2,
+    user: {
+      name: 'Anna',
+      avatarUrl: 'img/avatar-angelina.jpg',
+    },
+    rating: 5,
+    comment:
+      'The house is amazing! Everything was clean, the host was super nice.',
+    date: '2024-06-15',
+  },
+];
