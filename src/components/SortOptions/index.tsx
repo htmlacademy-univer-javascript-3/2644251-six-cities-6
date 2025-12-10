30e76929c053d41bd1de50f474002f7ee582288d
alyona.belova <magic.near@yandex.ru>
2025-12-10 23:48:12 +0300

    feat: add offers sorting and map marker highlighting on hover

---
+++ src/components/SortOptions/index.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+type SortOptionsProps = {
+  value: string;
+  onChange: (newValue: string) => void;
+};
+
+const SORTING = [
+  { value: 'Popular', label: 'Popular' },
+  { value: 'PriceLowHigh', label: 'Price: low to high' },
+  { value: 'PriceHighLow', label: 'Price: high to low' },
+  { value: 'TopRated', label: 'Top rated first' },
+];
+
+const SortOptions = ({ value, onChange }: SortOptionsProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelect = (newValue: string) => {
+    onChange(newValue);
+    setIsOpen(false);
+  };
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by </span>
+
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {SORTING.find((s) => s.value === value)?.label}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+
+      <ul
+        className={`places__options places__options--custom${
+          isOpen ? ' places__options--opened' : ''
+        }`}
+      >
+        {SORTING.map((item) => (
+          <li
+            key={item.value}
+            className={`places__option${
+              item.value === value ? ' places__option--active' : ''
+            }`}
+            tabIndex={0}
+            onClick={() => handleSelect(item.value)}
+          >
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
+export default SortOptions;
